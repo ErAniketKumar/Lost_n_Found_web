239618a335a60e8471b9286aff51f7910b41f4e7
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-10-09 00:03:45 +0530

    updated item controller

--- backend/controller/ItemController.js
+++ backend/controller/ItemController.js
@@ -14,11 +14,10 @@ const AddItem = asyncHandler(async (req, res) => {
 		description,
 	} = req.body;
 
-	
-		if (!title || !category || !itemLocation || !imageUrl) {
-			res.status(500).json({ message: "All input are Required" });
-		}
-        try {
+	if (!title || !category || !itemLocation || !imageUrl) {
+		res.status(500).json({ message: "All input are Required" });
+	}
+	try {
 		const itemRecord = new itemModel({
 			title,
 			category,
@@ -36,68 +35,124 @@ const AddItem = asyncHandler(async (req, res) => {
 	}
 });
 
-
 //show single/specific items details
 const ItemInfo = asyncHandler(async (req, res) => {
-    const {id} = req.params;
-    try{
-        const itemInfo = await itemModel.findById({_id:id});
-        if(itemInfo) {
-            res.status(200).json(itemInfo);
-        } else {
-            res.status(404).json({message:"some error to fetch details!"});
-        }
-    } catch(error) {
-        console.log(error);
-        res.status(500).json({message:error});
-    }
-})
-
+	const { id } = req.params;
+	try {
+		const itemInfo = await itemModel.findById({ _id: id });
+		if (itemInfo) {
+			res.status(200).json(itemInfo);
+		} else {
+			res.status(404).json({ message: "no record found" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error });
+	}
+});
 
 // show all items record;
 
-const ShowAllItem = asyncHandler(async(req, res)=>{
-    try{    
-        const allitem = await itemModel.find({});
-        if(allitem) {
-            res.status(200).json(allitem);
-        } else {
-            res.status(500).json({message:"error to fetching"});
-        }
-    } catch(error) {
-        res.status(500).json({message:`internal issue: ${error}`});
-    }
+const ShowAllItem = asyncHandler(async (req, res) => {
+	try {
+		const allitem = await itemModel.find({});
+		if (allitem) {
+			res.status(200).json(allitem);
+		} else {
+			res.status(500).json({ message: "error to fetching not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: `internal issue: ${error}` });
+	}
 });
 
 // by category
 
 const ItemByCategory = asyncHandler(async (req, res) => {
-    const {category} = req.params;
-    try{
-        let itemInfo = await itemModel.find({});
+	const { category } = req.params;
+	try {
+		let itemInfo = await itemModel.find({});
 
-        if(itemInfo) {
-            console.log("itemInfo");
-            res.status(200).json(itemInfo);
-        } else {
-            res.status(404).json({message:"some error to fetch details!"});
-        }
-    } catch(error) {
-        console.log(error);
-        res.status(500).json({message:error});
-    }
-})
+		if (itemInfo) {
+			const filteredItem = itemInfo.filter((item) => {
+				return item.category == category;
+			});
+			if (filteredItem.length > 0) {
+				res.status(200).json(filteredItem);
+			} else {
+				res.status(404).json({ message: "No item in this Category" });
+			}
+		} else {
+			res
+				.status(404)
+				.json({ message: "Some error occurred while fetching details!" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+});
 
+const DeleteItem = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	try {
+		const item = await itemModel.findByIdAndDelete({ _id: id });
+		if (item) {
+			res.status(200).json({ message: "item deleted successfully" });
+		} else {
+			res.status(404).json({ message: "item not found" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error });
+	}
+});
 
-
-
-
-
-
+const UpdateItem = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	const {
+		title,
+		category,
+		itemLocation,
+		dateTime,
+		imageUrl,
+		currentAddress,
+		description,
+	} = req.body;
+	try {
+		const itemInfo = await itemModel.findByIdAndUpdate(
+			id,
+			{
+				$set: {
+					title,
+					category,
+					itemLocation,
+					dateTime,
+					imageUrl,
+					currentAddress,
+					description,
+				},
+			},
+			{ new: true, runValidators: true }
+		);
+		if (itemInfo) {
+			res
+				.status(200)
+				.json({ message: "Item updated successfully", item: itemInfo });
+		} else {
+			res.status(404).json({ message: "somthing went wrong" });
+		}
+	} catch (error) {
+		console.error("Update error:", error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+});
 
 module.exports = {
 	AddItem,
-    ItemInfo,
-    ShowAllItem,
-
+	ItemInfo,
+	ShowAllItem,
+	ItemByCategory,
+	DeleteItem,
+	UpdateItem,
 };
--- backend/routes/userAuthRoute.js
+++ backend/routes/userAuthRoute.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const { Login, Signup, Logout } = require("../controller/AuthController");
 
-const {AddItem,ItemInfo,ShowAllItem}  = require("../controller/ItemController");
-
-
+const {
+	AddItem,
+	ItemInfo,
+	ShowAllItem,
+	ItemByCategory,
+	DeleteItem,
+    UpdateItem
+} = require("../controller/ItemController");
 
 //auth controller
 router.post("/login", Login);
@@ -14,16 +19,19 @@ router.post("/signup", Signup);
 
 router.post("/logout", Logout);
 
-
 //auth controllerres.end
 
-
-
 //items controller start
-router.post("/additem",AddItem);
+router.post("/additem", AddItem);
 
-router.get("/allitem",ShowAllItem);
+router.get("/allitem", ShowAllItem);
 
-router.get("/item/:id",ItemInfo);
+router.get("/item/:id", ItemInfo);
+
+router.get("/item/category/:category", ItemByCategory);
+
+router.delete("/item/:id",DeleteItem);
+
+router.put("/item/:id",UpdateItem)
 
 module.exports = router;
