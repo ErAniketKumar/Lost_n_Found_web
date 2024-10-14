d8614a176740f481c63ebb01b20d29a6c496e38f
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-10-15 01:12:50 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,15 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
+const allowedOrigins = [
+  'https://https://mayihelpyou-mq14ts3k8-anikets-projects-3e38dd37.vercel.app', // Your deployed frontend URL
+  'http://localhost:5173',            // Localhost for development
+];
+
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
-    credentials: true, // Allow credentials
-  }));
+  origin: allowedOrigins,
+  credentials: true // Allow credentials if needed
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
